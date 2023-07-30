5a9ee03bb0e2fc7a08da65c82b4c2468283f19f4
Siphon880gh <weng.f.fung@gmail.com>
2023-07-29 22:31:30 -0700

    ChatGPT 3.5 model initialized

---
+++ script.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { OpenAI } = require('langchain/llms/openai');
+
+const apiKey = process.env.OPENAI_API_KEY;
+
+const model = new OpenAI({ 
+    openAIApiKey: process.env.OPENAI_API_KEY, 
+    temperature: 0,
+    model: 'gpt-3.5-turbo'
+});
+  
+console.log({ model });

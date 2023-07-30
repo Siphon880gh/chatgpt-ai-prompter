eaefe7795d8628d7b56964ea35ba6c5cc2f72871
Siphon880gh <weng.f.fung@gmail.com>
2023-07-29 22:34:42 -0700

    Added JsDocs Comments

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const { OpenAI } = require('langchain/llms/openai');
 
 const apiKey = process.env.OPENAI_API_KEY;
 
+/**
+ * OpenAI constructor
+ * @constructor
+ * @param {Object} options - Options for OpenAI API
+ * @param {int} options.apiKey - The temperature property represents variability in the words selected in a response. Temperature ranges from 0 to 1 with 0 meaning higher precision but less creativity and 1 meaning lower precision but more variation and creativity.
+ * 
+ */
 const model = new OpenAI({ 
     openAIApiKey: process.env.OPENAI_API_KEY, 
     temperature: 0,
     model: 'gpt-3.5-turbo'
 });
-  
-console.log({ model });
+// console.log({ model });
